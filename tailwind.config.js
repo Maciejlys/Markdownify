3b1edd1ee59f529475283e47b7d6c6969c90b721
maciej.lyszczarz <maciej.lyszczarz@jamf.com>
2023-08-02 00:42:04 +0200

    colors moved to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				purple: 'rgb(123, 31, 162)',
+				violet: 'rgb(103, 58, 183)',
+				pink: 'rgb(244, 143, 177)',
+				lightGrey: 'rgb(227, 227, 227)',
+			},
+		},
 	},
 	plugins: [],
 };
